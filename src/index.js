ca8455bc1dbee9aed5f913d7d6a05d8e9da1c6fa
annacarolins <anafrontend@hotmail.com>
2025-05-04 16:49:24 -0300

    Adiciona funcionalidade de cálculo de conta e desconto

--- src/index.js
+++ src/index.js
@@ -9,9 +9,20 @@ const valorTotal = readline.questionFloat('Digite o valor total da conta: ');
 
 // Capturando o método de pagamento
 const metodoPagamento = readline.question('Qual é o método de pagamento (PIX, dinheiro ou cartão)? ');
+let valorComDesconto = valorTotal;
 
+if (metodoPagamento.toLowerCase() === 'pix' || metodoPagamento.toLowerCase() === 'dinheiro') {
+  valorComDesconto = valorTotal * 0.9;
+}
+// Calculando o valor por pessoa e exibindo os resultados
+const valorPorPessoa = valorComDesconto / numeroPessoas;
 
-//Façã sua lógica para aplicar o desconto apenas para PIX OU DINHEIRO
+console.log('\nResumo da conta:');
+console.log('Número de pessoas na mesa:', numeroPessoas);
+console.log('Método de pagamento:', metodoPagamento);
+console.log('Valor total com desconto: R$', valorComDesconto.toFixed(2));
+console.log('Cada pessoa deve pagar: R$', valorPorPessoa.toFixed(2));
+
 
 
 // Exibindo os resultados
